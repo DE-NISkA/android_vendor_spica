a9f8313b325481693656028993682581edec015e
DE-NISkA <DE-NISkA@gmail.com>
2010-09-30 21:37:23 +0600

    Add source for akmd service

---
+++ GT-I5700/akmd/Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_ARM_MODE := arm
+LOCAL_SRC_FILES := Akmd.cpp Calibrator.cpp main.cpp \
+    math/Matrix.cpp math/Vector.cpp \
+    device/AKM8973_2_6_29.cpp \
+    device/BMA150.cpp \
+    device/OrientationAdapter.cpp device/TemperatureReaderAdapter.cpp
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_MODULE := akmd.free
+
+include $(BUILD_EXECUTABLE)
